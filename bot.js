45b0738a1331d6b67d181246bdfc82e8938e7161
abush46 <40731418+abush46@users.noreply.github.com>
2024-12-24 00:13:00 +0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,12 @@
+const TelegramBot = require("node-telegram-bot-api");
+const token = "7242228033:AAGeQDRDv5Texj6aLM586TMbdiZcZy2gd_8"; // Replace with your own bot token
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, "Welcome to the bot!");
+  }
+});
